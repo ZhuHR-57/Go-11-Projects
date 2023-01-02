123927d10119d12ce393d4b33dfa624110adb141
Lido <zhuhr_57@yeah.net>
2023-01-02 16:21:50 +0800

    update

--- p2-Go-CURD/main.go
+++ p2-Go-CURD/main.go
@@ -13,19 +13,21 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Book struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
 
 type Author struct {
 	Name string `json:"name"`
-	Age int `json:"age"`
+	Age  int    `json:"age"`
 }
 
 var books []Book
@@ -35,20 +37,20 @@ var books []Book
 // @Description 创建一些书籍，用于测试
 // @Author lido 2023-01-02 11:18:10
 //
-func initBooks(){
-	books = append(books,Book{
-		ID:     "1",
-		Isbn:   "1234",
-		Title:  "Book Title 1",
+func initBooks() {
+	books = append(books, Book{
+		ID:    "1",
+		Isbn:  "1234",
+		Title: "Book Title 1",
 		Author: &Author{
 			Name: "author 1",
 			Age:  1,
 		},
 	})
-	books = append(books,Book{
-		ID:     "2",
-		Isbn:   "1235",
-		Title:  "Book Title 2",
+	books = append(books, Book{
+		ID:    "2",
+		Isbn:  "1235",
+		Title: "Book Title 2",
 		Author: &Author{
 			Name: "author 2",
 			Age:  2,
@@ -65,24 +67,88 @@ func main() {
 	//多路复用
 	r := mux.NewRouter()
 
-	r.HandleFunc("/books",getBooks).Methods("GET")
-	r.HandleFunc("/books/{id}",getbook).Methods("GET")
-	r.HandleFunc("/books",createBook).Methods("POST")
-	r.HandleFunc("/books/{id}",updateBook).Methods("PUT")
-	r.HandleFunc("/books/{id}",deleteBook).Methods("DELETE")
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getbook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
-	log.Print("Starting server at port 8080")
+	log.Print("Starting server at port 8000")
 
-	if err := http.ListenAndServe(":8081",nil); err != nil{
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(books)
-
-
+	return
 }
 
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	//1. 设置json头
+	w.Header().Set("Content-Type", "application/json")
+	//2. 获取参数
+	params := mux.Vars(r)
+	//3. 找到需要删除的ID并删除
+	for index, item := range books {
+		if item.ID == params["id"] {
+			// 删除指定的元素
+			books = append(books[:index], books[index+1:]...)
+			return
+		}
+	}
+	return
+}
+
+func getbook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	return
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var book Book
+
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+
+	return
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// 找到元素并删除
+	for index, item := range books {
+		if item.ID == params["id"] {
+			// 1. 删除原来的书
+			books = append(books[:index], books[index+1:]...)
+			// 2. 建立新的书
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"] // 原来的ID不变
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	return
+}
---
+++ someTips.md
@@ -0,0 +1,47 @@
+> 1. 删除指定的slice元素
+
+```go
+slice = append(slice[:index],slice[index+1:]...)
+```
+
+> 2. json方法的选择
+>
+> + json.NewDecoder用于http连接与socket连接的读取与写入，或者文件读取；（性能高）
+> + json.Unmarshal用于直接是byte的输入。
+
+##### `1：json.Unmarshal进行解码`
+
+```go
+func HandleUse(w http.ResponseWriter, r *http.Request) {
+    var u Use //此处的Use是一个结构体
+    data, err := ioutil.ReadAll(r.Body)//此处的r是http请求得到的json格式数据-->然后转化为[]byte格式数据.
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    if err := json.Unmarshal(data, &u); err != nil { //经过这一步将json解码赋值给结构体，由json转化为结构体数据
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "姓名：%s，年龄：%d", u.Name, u.Age)
+
+}
+123456789101112131415
+```
+
+##### `2. json.NewDecoder解码`
+
+```go
+func HandleUse(w http.ResponseWriter, r *http.Request) {
+    var u Use
+    if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "姓名：%s，年龄：%d", u.Name, u.Age)
+
+}
+123456789
+```
